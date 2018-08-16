2b35263fbc7eb59d1416082e0cab147553314955
Henk Griffioen <hgrif@users.noreply.github.com>
2018-08-16 10:10:57 +0200

    Update __init__.py

--- exampleproject/__init__.py
+++ exampleproject/__init__.py
@@ -1,2 +1,6 @@
+# See https://docs.python-guide.org/writing/logging/#logging-in-a-library
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 from . import data
 from . import plot
